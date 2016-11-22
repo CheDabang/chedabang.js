f3b15ce700d6ee1434c837749b703b0fffd0f52b
车大棒 <che_dabang@qq.com>
2016-11-22 16:21:38 +0800

    添加了柱状图
    在添加柱状图的时候，矩形总是画的有偏差。通过检查发现，原来是折线图修改data数据的时候，把原始的data的值修改了。后面在原型方法当中，重新new
    Object，然后通过点语法复制data数据

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ###  使用说明：
 1、折线图：
 ```
- new Line(data,id); 语法的格式
+ new Line(data,id);  语法格式
 ```
 **具体实例**
 ```
@@ -29,10 +29,9 @@
 </script>
 ```
 
-这里首先按照此格式设置一个折线图的数据点，之后实例化Line构造函数。传入data数据，和你的canvasID值。之后就能够生成一个折线图。
-2、饼状图：
+#### 2、饼状图：
 ```
-new Pie(data,id);语法的格式
+new Pie(data,id);   语法格式
 ```
 **具体实例**
 ```
@@ -54,3 +53,26 @@ new Pie(data,id);语法的格式
 	var pie = new pie(data, "#pie");
 </script>
 ```
+
+#### 3、柱状图：
+```
+new Bar(data,id);   语法格式
+```
+**具体实例**
+<script type="text/javascript">
+	var data = {
+		0: 260,
+		1: 200,
+		2: 168,
+		3: 450,
+		4: 360,
+		5: 320,
+		6: 237,
+		7: 189,
+		8: 257,
+		9: 389,
+		10: 289,
+		11: 236
+	}
+	var bar = new Bar(data, "#bar");
+</script>
--- js/chedabang.js
+++ js/chedabang.js
@@ -46,18 +46,20 @@ function $(str) {
 function Line(data, id) {
 	this.draw = $(id).getContext("2d");
 	this.data = data;
-	for(var k in data){
-		this.data[k] = parseInt($(id).offsetHeight - 50 - data[k])
-	}
-	//这里的for循环恨重要，因为canvas的坐标轴起始点和平时咱们见到的折线图坐标不一样的。
-	//所以需要for循环对这个数组值进行倒换。
-	//（说实话这个超级简单的问题，根本就是小学生的算术，我当时第一想到却是利用排序。看到知识会的多也不是好事呀）
+	this.id = $(id);
 	this.drawLine();
-	this.point();
 }
 Line.prototype = {
 	contructor: Line,
 	drawLine: function() {
+		var data1 = new Object; //实例化一个data1，避免影响data。
+		for(var k in data) {
+			data1[k] = parseInt(this.id.offsetHeight - 50 - data[k]);
+		}
+		//补充一点，把这个玩意放到里面，然后再实例化。就不会造成原始的data数据修改。
+		//这里的for循环恨重要，因为canvas的坐标轴起始点和平时咱们见到的折线图坐标不一样的。
+		//所以需要for循环对这个数组值进行倒换。
+		//（说实话这个超级简单的问题，根本就是小学生的算术，我当时第一想到却是利用排序。看到知识会的多也不是好事呀）
 		var draw = this.draw;
 		//开始绘制路径
 		draw.beginPath();
@@ -86,8 +88,8 @@ Line.prototype = {
 		draw.fillText("0", 45, 566);
 		draw.fillText("Y轴", 50, 45);
 		draw.fillText("X轴", 916, 555)
-		//这些坐标轴以后估计得简化一下，这个轴线的浪费的代码太少了。
-		//绘制坐标轴辅助线
+			//这些坐标轴以后估计得简化一下，这个轴线的浪费的代码太少了。
+			//绘制坐标轴辅助线
 		var x1 = 50;
 		var x2 = 850;
 		var x3 = 50;
@@ -98,16 +100,13 @@ Line.prototype = {
 		//折线加点
 		var x = 100;
 		var k1 = 0;
-		for(var k in data) {
+		for(var k in data1) {
 			if(k == 0) {
-				draw.moveTo(x, data[k]);
-				draw.arc(x, data[k], 5, 0, 2 * Math.PI, false);
+				draw.moveTo(x, data1[k]);
 			} else {
-				draw.lineTo(x, data[k]);
-				draw.arc(x, data[k], 5, 0, 2 * Math.PI, false);
+				draw.lineTo(x, data1[k]);
 			}
 			x += 60;
-			var k1 = parseInt(k) + 1;
 
 		}
 
@@ -131,26 +130,25 @@ Line.prototype = {
 		draw.stroke();
 		draw.closePath();
 		draw.save();
-	},
-	//下面的是画折线的圆点
-	point: function() {
-		var draw = this.draw;
-					var x = 100;
-					for(var k in data) {
-						draw.beginPath();
-						draw.fillStyle = "red";
-						if(k == 0) {
-							draw.arc(x, data[k], 5, 0, 2 * Math.PI, true);
-						} else {
-							draw.arc(x, data[k], 5, 0, 2 * Math.PI, true);
-						}
-						x += 60;
-						draw.closePath();
-						draw.fill();
-						//这里一定要注意呀，这个每画一个点都必须开始路径，并结束。否则颜色填充会填充出一大堆问题。
-					}
+		
+		//画坐标小圆点
+		var x = 100;
+		for(var k in data1) {
+			draw.beginPath();
+			draw.fillStyle = "red";
+			if(k == 0) {
+				draw.arc(x, data1[k], 5, 0, 2 * Math.PI, true);
+			} else {
+				draw.arc(x, data1[k], 5, 0, 2 * Math.PI, true);
+			}
+			x += 60;
+			draw.closePath();
+			draw.fill();
+			//这里一定要注意呀，这个每画一个点都必须开始路径，并结束。否则颜色填充会填充出一大堆问题。
+		}
 	}
 }
+
 function Pie(data, id) {
 	this.draw = $(id).getContext("2d");
 	this.data = data;
@@ -164,7 +162,7 @@ Pie.prototype = {
 		for(var k in data) {
 			num += parseInt(data[k]);
 		}
-		colors = ["blue", "#49be38", "red", "green", "yellow", "pink","#ff3365","orange","purple"]
+		colors = ["blue", "#49be38", "red", "green", "yellow", "pink", "#ff3365", "orange", "purple"]
 		var startAngle = 0;
 		var endAngle = 0;
 		for(var k in data) {
@@ -178,18 +176,105 @@ Pie.prototype = {
 			draw.fill();
 			startAngle = endAngle;
 			//绘制文字和形状
-			draw.fillRect(700,30*(parseInt(k) + 1),50,20);
+			draw.fillRect(700, 30 * (parseInt(k) + 1), 50, 20);
 			draw.font = "bold 16px Microsoft Yahei";
-			draw.textBaseline = "top";//设置文本基线，一个冷门知识点。主要是因为老外的英语
-			draw.moveTo(800,30);
-			draw.fillText("第"+(parseInt(k)+1)+"个月份额",760,30*(parseInt(k)+1));
-			var percent = Math.round((data[k]/num)*10000)/100 + "%";
+			draw.textBaseline = "top"; //设置文本基线，一个冷门知识点。主要是因为老外的英语
+			draw.moveTo(800, 30);
+			draw.fillText("第" + (parseInt(k) + 1) + "个月份额", 760, 30 * (parseInt(k) + 1));
+			var percent = Math.round((data[k] / num) * 10000) / 100 + "%";
 			//var percent =(data[k]/num).toFixed(4) + "%";  原本这样能够得到四位小数点，但是一旦再乘上100，那么就会出问题。有几个数小数位变的很长。
-			draw.fillText(percent,870,30*(parseInt(k)+1));
+			draw.fillText(percent, 870, 30 * (parseInt(k) + 1));
 			//绘制矩形
 			/*var ha = 0.034000000000001.toFixed(4)*100;
 			console.log(ha);   例如这里输出就是3.4000000000000004*/
-			
+
+		}
+	}
+}
+
+
+//绘制柱状图
+function Bar(data, id) {
+	this.draw = $(id).getContext("2d");
+	this.data = data;
+	console.log(this.data);
+	this.id = $(id);
+	this.bar();
+}
+Bar.prototype = {
+	contructor: Bar,
+	bar: function() {
+		var draw = this.draw;
+		//开始绘制路径
+		draw.beginPath();
+
+		//绘制Y轴
+		draw.moveTo(50, 550);
+		draw.lineTo(50, 50);
+
+		//绘制Y轴箭头
+		draw.moveTo(45, 60);
+		draw.lineTo(50, 50);
+		draw.lineTo(55, 60);
+
+		//绘制x轴
+		draw.moveTo(50, 550);
+		draw.lineTo(900, 550);
+
+		//绘制X轴箭头
+		draw.moveTo(890, 545);
+		draw.lineTo(900, 550);
+		draw.lineTo(890, 555);
+
+		//设立原点0，以及x轴、y轴
+		draw.font = "bold 16px Microsoft Yahei"
+		draw.textAlign = "center";
+		draw.fillText("0", 45, 566);
+		draw.fillText("Y轴", 50, 50);
+		draw.fillText("X轴", 916, 550)
+
+		//绘制坐标轴辅助线
+		var x1 = 50;
+		var x2 = 850;
+		var x3 = 50;
+		var y = 550;
+		var yt = 0;
+		var xt = 0;
+
+		//绘制基本辅助线
+		for(var i = 1; i <= 9; i++) {
+			y -= 50;
+			yt += 50;
+			draw.moveTo(x1, y);
+			draw.lineTo(x2, y);
+			draw.textAlign = "end";
+			draw.fillText(yt, x1, y);
+			//draw.textAlign = "start";
+		}
+
+		//绘制底部轴
+		for(var i = 1; i <= 12; i++) {
+			xt++;
+			x3 += 60;
+			draw.fillText(xt + "月", x3, 566);
+		}
+		draw.stroke();
+		draw.closePath();
+		draw.save();
+
+		//绘制矩形
+		var data1 = new Object;
+		for(var k in data) {
+		data1[k] = parseInt(this.id.offsetHeight - 50 - data[k]);
+		}
+		var x = 80;
+		for(var k in this.data) {
+			draw.beginPath();
+			draw.fillStyle = "#008cd6";
+			draw.fillRect(x, data1[k],30,data[k]);
+			draw.closePath();
+			x += 60;
+
 		}
 	}
 }
